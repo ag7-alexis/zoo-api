e3038093d32daabadbdf79fb3af362f895c98cf5
ag7-alexis <guay.alexis7@gmail.com>
2022-07-06 10:35:57 +0200

    switch to import

--- routes/routesCustomer.js
+++ routes/routesCustomer.js
@@ -1,30 +1,29 @@
-const express = require('express');
-const Customer = require('./models/Customer');
+import express from "express";
+import Customer from "../models/customer.js";
 
 const router = express.Router();
 
-module.exports = router;
+export default router;
 
-router.get('/customer', async (req, res) => {
+router.get("/customer", async (req, res) => {
     const customer = await Customer.find();
     res.send(customer);
-})
+});
 
-router.post('/customer', async (req, res) => {
+router.post("/customer", async (req, res) => {
     const customer = new Customer({
         name: req.body.name,
         age: req.body.age,
         email: req.body.email,
         adress: req.body.adress,
         phone: req.body.phone,
-        peoples: { 
+        peoples: {
             Age: req.body.peoples.Age,
-            Sexe: req.body.peoples.Sexe
-        }
-    })
+            Sexe: req.body.peoples.Sexe,
+        },
+    });
 
     await dog.save();
 
     res.send(dog);
-})
-
+});
