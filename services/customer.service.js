d60c60607fd66a2d25e917c18968b56e9550a29f
ag7-alexis <guay.alexis7@gmail.com>
2022-07-07 12:54:42 +0200

    retrieve sexe repartition

--- services/customer.service.js
+++ services/customer.service.js
@@ -1,3 +1,4 @@
+import e from "express";
 import Customer from "../models/customer.model.js";
 
 export const getCustomers = (filter, page, limit) => {
@@ -29,34 +30,22 @@ export const getCustomerById = (id) => {
 
 export const getCustomerEventsById = (id) => {
     try {
-        return Customer.findById(id, {"events": 1} );
+        return Customer.findById(id, { events: 1 });
     } catch (error) {
         throw Error("Fail when try to find Customer with Id " + id);
     }
 };
 
-export const getMinorCustomer = (filter) => {
+export const countPeopleBySexe = () => {
     try {
-        //return Customer.count({"peoples.Age": { $gt: 0, $lt: 18 }});
-        //return Customer.count({peoples: { $elemMatch: { Age: { $gt: 0, $lt: 18 } } }});
         return Customer.aggregate([
-            { $match: { /* Query can go here, if you want to filter results. */ } } 
-          , { $group: { /* execute 'grouping' */
-                  Age: { $gt: 0, $lt: 18 } /* using the 'token' value as the _id */
-                , count: { $sum: 1 } /* create a sum value */
-              }
-            }
+            {
+                $match: {},
+            },
+            { $project: { peoples: 1 } },
+            { $unwind: "$peoples" },
+            { $group: { _id: "$peoples.Sexe", count: { $sum: 1 } } },
         ]);
-    } catch (error) {
-        throw Error("Fail when try to find Customer major or minor");
-    }
-};
-
-export const getMajorCustomer = (filter) => {
-    try {
-        return Customer.count({"peoples.Age": { $gt: 18, $lt: 99 }});
-
-        //db.customers.find({"peoples.Age": { $gt: 18, $lt: 23 }})
     } catch (error) {
         throw Error("Fail when try to find Customer major or minor");
     }
